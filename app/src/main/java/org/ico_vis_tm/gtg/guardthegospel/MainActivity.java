17295f3ee704f42b135c1fc1fbf9660bf5dc7728
Jonas Tandinco <mymyjonas@gmail.com>
2018-04-13 11:33:28 +0800

    initial project files

---
+++ app/src/main/java/org/ico_vis_tm/gtg/guardthegospel/MainActivity.java
@@ -0,0 +1,13 @@
+package org.ico_vis_tm.gtg.guardthegospel;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
